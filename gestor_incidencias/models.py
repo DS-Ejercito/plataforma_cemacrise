c4a4d5be695a9dad2fa21908fc5dba054061b898
Devm94 <116216613+Devm94@users.noreply.github.com>
2024-11-17 23:49:01 -0600

    Logica_1

---
+++ gestor_incidencias/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+class procedencia(models.Model):
+    id = models.AutoField(primary_key=True)
+    descrip_corta = models.TextField()    
+    descrip_larga = models.TextField()    
+    cod_proced_superior = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategorias')
+    def __str__(self):
+        fila =  str(self.descrip_corta)
+        return fila
+    
+class tp_incidencia(models.Model):
+    id = models.AutoField(primary_key=True)
+    descrip_corta = models.TextField()    
+    def __str__(self):
+        fila =  str(self.descrip_corta)
+        return fila
+    
+class depto(models.Model):
+    id = models.AutoField(primary_key=True)
+    descrip_corta = models.TextField()
+    def __str__(self):
+        fila = str(self.descrip_corta)
+        return fila
+
+class munic(models.Model):
+    id = models.AutoField(primary_key=True)
+    descrip_corta = models.TextField()
+    cod_depto = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategorias')
+    def __str__(self):
+        fila = str(self.descrip_corta)
+        return fila
+
+class incidencia(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    procedencia = models.ForeignKey(procedencia, on_delete=models.CASCADE)
+    tp_incidencia = models.ForeignKey(procedencia, on_delete=models.CASCADE)
+    depto = models.ForeignKey(procedencia, on_delete=models.CASCADE)
+    munic = models.ForeignKey(procedencia, on_delete=models.CASCADE)
+    obser = models.TextField(blank=True, null=True)
+    longitud = models.FloatField(null = True)
+    latitud = models.FloatField(null = True)
+    cant = models.FloatField(null = True)
+    img = models.FileField(upload_to='Incidencias/', null = True, blank= True)
+    def __str__(self):
+        fila = "- Descripcion:" + str(self.fecha)
+        return fila
+    
